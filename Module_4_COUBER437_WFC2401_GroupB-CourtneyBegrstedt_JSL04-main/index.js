7712d867b97fd492524e6219da711db034c01d6e
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-03-21 21:11:50 +0200

    Update index.js
    Took away the back tics from line 12 , 23 and 28

--- Module_4_COUBER437_WFC2401_GroupB-CourtneyBegrstedt_JSL04-main/index.js
+++ Module_4_COUBER437_WFC2401_GroupB-CourtneyBegrstedt_JSL04-main/index.js
@@ -9,7 +9,7 @@ console.log(flavorSelection);
 
 
 let cansLeft = 3; // Any number of cans
-console.log(`cansLeft < 5 ? "Time to restock" : "We're stocked!"`);
+console.log(cansLeft < 5 ? 'Time to restock' : 'We're stocked!');
 
 
 
@@ -20,18 +20,15 @@ console.log(heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!');
 
 
 let currentTemp = 4; // Current temperature in °C
-console.log(`currentTemp <= 5 ? Chilled to perfection! : Needs a cooler!`);
+console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
 
 // Define the current hour in 24-hour format
 let currentHour = 22; 
-console.log(`currentHour >= 7 && currentHour < 24 ? Unleash the beast! : Better stick to water.`)
+console.log(currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.')
 
 
 
 
 
 
-// In each of these challenges, the ternary operator is used to decide between two options based on a condition, 
-// demonstrating its utility for concise conditional logic. Remember that while ternaries can make the code more succinct, 
-// they should be used judiciously to maintain readability, especially for more complex conditions.
