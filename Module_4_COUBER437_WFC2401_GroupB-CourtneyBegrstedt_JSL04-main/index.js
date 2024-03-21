0cb42a46f686ae7acc5763fc7d7fa4a4cab798bb
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-03-22 01:08:15 +0200

    Update index.js
    Added code comments and edited a few code lines

--- Module_4_COUBER437_WFC2401_GroupB-CourtneyBegrstedt_JSL04-main/index.js
+++ Module_4_COUBER437_WFC2401_GroupB-CourtneyBegrstedt_JSL04-main/index.js
@@ -3,13 +3,14 @@
 
 
 let userPreference = 'Sugar-free'; // Possible values: 'Regular', 'Sugar-free', or undefined
-let flavorSelection = userPreference ? userPreference : 'Regular';
+let flavorSelection = userPreference ? userPreference : 'Regular'; //If the user preference is not set console.log will output 'Regular' 
 console.log(flavorSelection);
 
 
 
 let cansLeft = 3; // Any number of cans
-console.log(cansLeft < 5 ? 'Time to restock' : 'We're stocked!');
+let restockMessage = (cansLeft < 5) ? 'Time to restock' : 'We stocked!'; // A message will be assigned depending on the condition
+console.log(restockMessage); 
 
 
 
@@ -24,7 +25,7 @@ let currentTemp = 4; // Current temperature in °C
 console.log(currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!');
 
 
-// Define the current hour in 24-hour format
+// Current hour in 24-hour format
 let currentHour = 22; 
 console.log(currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.')
 
